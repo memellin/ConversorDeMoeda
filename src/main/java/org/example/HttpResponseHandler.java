2c14c6f4ee012c8d2698cfe50da3af48837e1d25
Enock <enocksm@outlook.com>
2024-07-16 18:52:38 -0300

    desafio concluido

---
+++ src/main/java/org/example/HttpResponseHandler.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpResponseHandler {
+    private int statusCode;
+    private String body;
+
+    public HttpResponseHandler(HttpResponse response) throws IOException {
+        this.statusCode = response.getStatusLine().getStatusCode();
+        HttpEntity entity = response.getEntity();
+        this.body = entity != null ? EntityUtils.toString(entity) : null;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
